dff0c6d8b23c99f218e255c395581a157526d4c9
DuyPhuongkhtn <39594416+DuyPhuongkhtn@users.noreply.github.com>
2024-01-14 10:52:24 +0700

    Beta (#1)
    * merge commit
    
    * B5
    
    * B6
    
    * B7
    
    * B7
    
    * B8
    
    * B9
    
    ---------
    
    Co-authored-by: PhuongDuy-PP <15520659@gm.uit.edu.vn>

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 console.log("learning github")
 
-// git status -> kiểm tra những file thay đổi nội dung
+// B1: git status -> kiểm tra những file thay đổi nội dung
 // hoặc file mới tạo
+
+//  B2: git add . => add toàn bộ những file thay đổi và những
+// file mới tạo
+// git add <path file>
+
+// B3: git commit -m "<nội dung commit>"
+
+// B4: git push origin <branch name>
+
+// tạo branch mới
+// git checkout -b <tên branch> <tên branch cơ sở>
+
+// commit B3
+
+// commit B4
+
+// commit B5
+
+// commit B6
